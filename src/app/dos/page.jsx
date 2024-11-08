171fe3ff6bf893acb6db6e47b3a90af9351a24c6
Celso Franciscano <celsofranciscano00@gmail.com>
2024-11-08 07:00:05 -0400

    Nueva version del proyecto

---
+++ src/app/dos/page.jsx
@@ -0,0 +1,12 @@
+export default function Paginadedo() {
+let nombre;
+const nombres= "Celso"
+
+
+  return (
+    <div className=" bg-black text-white">
+        {nombre}
+     
+    </div>
+  );
+}
